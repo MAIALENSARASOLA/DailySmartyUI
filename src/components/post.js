4e9e8a153f88f258efdd8e86417ec8960a776f3f
MAIALENSARASOLA <maialen.sarasola910@gmail.com>
2026-08-20 05:06:38 +0200

    added react-animate-height for post links hover effect

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      height: 0
+    }
+  }
+
   renderTopics() {
     const associatedTopics = this.props.associated_topics || [];
 
@@ -13,18 +22,55 @@ class Post extends Component {
     });
   }
 
-  render() {
-    return (
-      <li className="recent-post">
-        <div className="recent-post__title">
-          {this.props.title}
+  renderLinks() {
+    let links = this.props.post_links.map((post_link, index) => {
+      return (
+        <div className="post-link" key={index}>
+          <div className="post-link__box"></div>
+          <div className="post-link__link">
+            <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+          </div>
         </div>
+      );
+    });
+    return links;
+  }
 
-        <div className="recent-post__topics">
-          {this.renderTopics()}
-        </div>
-      </li>
-    );
+  render() {
+    if (this.props.type == 'recent') {
+      return (
+        <li className="recent-post">
+          <div className="recent-post__title">
+            {this.props.title}
+          </div>
+          <div className="recent-post__topics">
+            {this.renderTopics()}
+          </div>
+        </li>
+      );
+    } else if (this.props.type == 'result') {
+      return (
+        <li className="result-post"
+            onMouseEnter={() => this.setState({ height: 70 })}
+            onMouseLeave={() => this.setState({ height: 0 })}
+          >
+          <div className="result-post__topics">
+            {this.renderTopics()}
+          </div>
+          <div className="result-post__title">
+            {this.props.title}
+          </div>
+          <AnimateHeight
+            duration={500}
+            height={this.state.height}
+          >
+            <div className="result-post__links">
+              {this.renderLinks()}
+            </div>
+          </AnimateHeight>
+        </li>
+      );
+    }
   }
 }
 
