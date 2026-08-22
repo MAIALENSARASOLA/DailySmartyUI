cd9334ea969f4078affe3b27bc94ea1834df9ddd
MAIALENSARASOLA <maialen.sarasola910@gmail.com>
2026-08-22 16:20:49 +0200

    added no-content to post links when post links are empty for post, and applied flex styles to post-links

--- src/components/post.js
+++ src/components/post.js
@@ -52,7 +52,10 @@ class Post extends Component {
           </div>
         </div>
       );
-    });
+    })
+    if(links == 0) {
+        return <div className="no-content">No Post Links</div>
+    }
     return links;
   }
 
